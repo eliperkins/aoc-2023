f858275f4f388c377070053df48316f9ee42c0f2
Eli Perkins <eli.j.perkins@gmail.com>
2023-12-12 11:06:07 -0700

    Fix sorting

--- Scripts/download.sh
+++ Scripts/download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-day=$(ls Sources/AdventOfCode2023 | grep -E 'Day[0-9]+.swift' | tail -1 | grep -oE '[0-9]+')
+day=$(ls -1v Sources/AdventOfCode2023 | grep -E 'Day[0-9]+.swift' | sort -V | tail -1 | grep -oE '[0-9]+')
 for ((i=1; i<=day; i++)); do
     echo "Fetching day $i input..."
     swift run aoc-cli fetch -d $i
